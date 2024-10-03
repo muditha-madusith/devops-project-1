845b4bd630ec8b8548d7b228887174166841ef7c
Muditha Madusith <108977790+muditha-madusith@users.noreply.github.com>
2024-10-03 17:37:06 +0530

    Update remote_backend_s3.tf

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket-123456"
+    bucket = "devproj-muditha-1"
     key    = "devops-project-1/terraform.tfstate"
     region = "eu-central-1"
   }
